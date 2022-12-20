7c9783c38c14eddd28088e479d445793fb031eb1
Joseph Sanchez <josephsanchez@josephs-mbp.local>
2022-12-19 16:34:21 -0800

    testing sw file

---
+++ README.md
@@ -0,0 +1,37 @@
+# Text-Editor
+
+## Table of Contents:
+
+[1. Description](#Description)  
+[2. Installation](#Installation)  
+[3. Usage](#Usage)  
+[4. Output](#Output)  
+[5. Contributors](#Contributors)  
+[6. Questions](#Questions)
+
+## Description
+
+## Installation
+
+
+## Usage
+
+1. run "npm start" in terminal to begin
+
+## Output
+
+Link: <a href="https://tech-blog-sql.herokuapp.com" target="_blank">https://tech-blog-sql.herokuapp.com/</a>
+
+## Contributors
+
+Joeychez23
+
+## Questions
+
+Email: joeychez123@gmail.com
+
+Github: https://github.com/Joeychez23
+
+Repo: https://github.com/Joeychez23/tech-blog
+
+Link: <a href="https://tech-blog-sql.herokuapp.com" target="_blank">https://tech-blog-sql.herokuapp.com/</a>
--- client/src-sw.js
+++ client/src-sw.js
@@ -8,33 +8,47 @@ const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 precacheAndRoute(self.__WB_MANIFEST);
 
 
-//Create Cashe
-const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
-  plugins: [
-    new CacheableResponsePlugin({
-      statuses: [0, 200],
-    }),
-    new ExpirationPlugin({
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-  ],
-});
 
+//Create Cashe
+const pageCache = 
+  new CacheFirst({
+    cacheName: 'page-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      }),
+    ],
+  });
+
+//Passes the URLs though to the cache of page cache because they are API requests and they dont exist as files
 warmStrategyCache({
   urls: ['/index.html', '/'],
   strategy: pageCache,
 });
 
 
-// Dosen't cache system process files
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+//If the values passed into 'page-cache' are system files then request.mode = HTML navigation meaning 
+//that if i placed the bundle.js file inside the warmStrategyCache it wouldn't pass to the cache
+const pageCasheParams = registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+console.log(pageCasheParams);
 
-// TODO: Implement asset caching
-registerRoute( 
 
-  //Files to be cashes
-  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+//Files to be cashes
+const assetCasheParams = function ({ request }) {
+  return (
+    // CSS
+    request.destination === 'style' ||
+    // JavaScript
+    request.destination === 'script' ||
+
+    request.destination === 'worker'
+  );
+}
+registerRoute(
+  assetCasheParams,
   new StaleWhileRevalidate({
     cacheName: 'asset-cache',
     plugins: [
